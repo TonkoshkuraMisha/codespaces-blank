ac08e8ee3930fda5636f2aa137f3fb9529885985
Mykháilo Tonkoshkúra <tonkoshkura.misha@gmail.com>
2023-03-24 19:29:37 +0000

    quick-sort was added

---
+++ test1.py
@@ -0,0 +1,19 @@
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        left = []
+        right = []
+        for i in range(1, len(arr)):
+            if arr[i] < pivot:
+                left.append(arr[i])
+            else:
+                right.append(arr[i])
+        return quicksort(left) + [pivot] + quicksort(right)
+
+
+
+arr = [5, 3, 8, 4, 2]
+sorted_arr = quicksort(arr)
+print(sorted_arr)
